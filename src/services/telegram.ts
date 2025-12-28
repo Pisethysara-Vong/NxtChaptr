074ef7c4c0a9ee4ed1659e3f42d1aa1feb578afd
Pisedhysara Vong <pisedhysaravong@Pisedhysaras-MacBook-Pro.local>
2025-12-28 18:13:00 +0700

    remove console.log for .env variables

--- src/services/telegram.ts
+++ src/services/telegram.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 import { TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID } from "../config/env";
 
 export async function sendTelegramMessage(message: string): Promise<void> {
-  console.log("BOT_TOKEN:", process.env.TELEGRAM_BOT_TOKEN);
-  console.log("CHAT_ID:", process.env.TELEGRAM_CHAT_ID);
-
   const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
 
   await axios.post(url, {
